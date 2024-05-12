8b35a2a3f5dcf27abbf8d3b0ebafc709d93b150f
samuelncaetano <samueln.caetano42@gmail.com>
2024-05-12 19:10:04 -0300

    test(InMemoryBookRepository): implementation of the zod schema in tests

--- src/test/inMemoryCreateBooks/InMemoryBookRepository.spec.ts
+++ src/test/inMemoryCreateBooks/InMemoryBookRepository.spec.ts
@@ -15,7 +15,7 @@ describe("InMemoryCreateBooks", () => {
     const bookParams: InMemoryCreateBookParams = {
       title: "1984",
       author: "George Orwell",
-      volume: "1",
+      volume: 1,
       status: "STARTED",
     };
 
@@ -26,14 +26,29 @@ describe("InMemoryCreateBooks", () => {
     expect(createdBook).toBeDefined();
     expect(createdBook.title).toBe("1984");
     expect(createdBook.author).toBe("George Orwell");
-    expect(createdBook.volume).toBe("1");
+    expect(createdBook.volume).toBe(1);
     expect(createdBook.status).toBe("STARTED");
+  });
+  it("Must create a book in memory without any params", async () => {
+    // Arrange
+    const inMemoryCreateBooks = new InMemoryCreateBooks();
+    const bookParams: InMemoryCreateBookParams = {};
+
+    // Act
+    const createdBook = await inMemoryCreateBooks.createBook(bookParams);
+
+    // Assert
+    expect(createdBook).toBeDefined();
+    expect(createdBook.title).toBe(null);
+    expect(createdBook.author).toBe(null);
+    expect(createdBook.volume).toBe(null);
+    expect(createdBook.status).toBe("NOT_STARTED");
   });
 });
 
 // main/controllers/InMemoryCreateBookController.test.ts
 describe("InMemoryCreateBookController", () => {
-  it("must create a book using the controller", async () => {
+  it("should create a book and return a successful response", async () => {
     // Arrange
     const inMemoryCreateBooks = new InMemoryCreateBooks();
     const inMemoryCreateBookController = new InMemoryCreateBookController(
@@ -43,7 +58,7 @@ describe("InMemoryCreateBookController", () => {
       body: {
         title: "1984",
         author: "George Orwell",
-        volume: "1",
+        volume: 1,
         status: "STARTED",
       },
     };
@@ -54,8 +69,57 @@ describe("InMemoryCreateBookController", () => {
     expect(httpResponse.body).toEqual({
       title: "1984",
       author: "George Orwell",
-      volume: "1",
+      volume: 1,
       status: "STARTED",
     });
+  });
+
+  it("should return a bad request response if the request body is invalid", async () => {
+    // Arrange
+    const inMemoryCreateBooks = new InMemoryCreateBooks();
+    const inMemoryCreateBookController = new InMemoryCreateBookController(
+      inMemoryCreateBooks,
+    );
+    const httpRequest: HttpRequest<InMemoryCreateBookParams> = {
+      body: {
+        title: "1984",
+        // author: "George Orwell",
+        volume: 1,
+        status: "STARTED",
+      },
+    };
+    // Act
+    const httpResponse = await inMemoryCreateBookController.handle(httpRequest);
+
+    // Assert
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual("Field author is Required");
+  });
+
+  it("should return a server error response if an error occurs during book creation", async () => {
+    // Arrange
+    const inMemoryCreateBooks = new InMemoryCreateBooks();
+
+    vi.spyOn(inMemoryCreateBooks, "createBook").mockRejectedValueOnce(
+      new Error("Failed to create book"),
+    );
+
+    const inMemoryCreateBookController = new InMemoryCreateBookController(
+      inMemoryCreateBooks,
+    );
+
+    const httpRequest: HttpRequest<InMemoryCreateBookParams> = {
+      body: {
+        title: "1984",
+        author: "George Orwell",
+        volume: 1,
+        status: "STARTED",
+      },
+    };
+    // Act
+    const httpResponse = await inMemoryCreateBookController.handle(httpRequest);
+    // Assert
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual("Something went wrong.");
   });
 });
