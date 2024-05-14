970347470e3984cb7695608c60c66f3ca0ca992c
samuelncaetano <samueln.caetano42@gmail.com>
2024-05-14 14:57:09 -0300

    refactor(routes): implement methods to delete books on routes

--- src/main/routes/bookRoutes/BookRoutes.ts
+++ src/main/routes/bookRoutes/BookRoutes.ts
@@ -1,10 +1,18 @@
 import express from "express";
-import { createBook, getBook, getBooksById } from "./BookServiceRoutes";
+import {
+  createBook,
+  deleteBook,
+  deleteBooksById,
+  getBook,
+  getBooksById,
+} from "./BookServiceRoutes";
 
 const bookRoutes = express.Router();
 
 bookRoutes.post("/", createBook);
 bookRoutes.get("/", getBook);
 bookRoutes.get("/:id", getBooksById);
+bookRoutes.delete("/", deleteBook);
+bookRoutes.delete("/:id", deleteBooksById);
 
 export default bookRoutes;
--- src/main/routes/bookRoutes/BookServiceRoutes.ts
+++ src/main/routes/bookRoutes/BookServiceRoutes.ts
@@ -2,6 +2,8 @@ import {
   createBookController,
   getBooksByIdController,
   getBooksController,
+  deleteBookController,
+  deleteBooksByIdController,
 } from "@/main/config/BookFactory";
 import { Request, Response } from "express";
 
@@ -19,6 +21,22 @@ export async function getBook(_: Request, res: Response): Promise<void> {
 
 export async function getBooksById(req: Request, res: Response): Promise<void> {
   const { body, statusCode } = await getBooksByIdController.handle({
+    body: req.body,
+    params: req.params,
+  });
+  res.status(statusCode).send(body);
+}
+
+export async function deleteBook(_: Request, res: Response): Promise<void> {
+  const { body, statusCode } = await deleteBookController.handle();
+  res.status(statusCode).send(body);
+}
+
+export async function deleteBooksById(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  const { body, statusCode } = await deleteBooksByIdController.handle({
     body: req.body,
     params: req.params,
   });
