b9b7df0451039743b304b3d303a1e12fe6660035
samuelncaetano <samueln.caetano42@gmail.com>
2024-05-14 14:56:28 -0300

    refactor(BookFactory): factory update for methods to delete books

--- src/main/config/BookFactory.ts
+++ src/main/config/BookFactory.ts
@@ -1,17 +1,37 @@
 import { PrismaCreateBooks } from "@/infrastructure/database/prisma/prismaCreateBooks";
 import { PrismaGetBooks } from "@/infrastructure/database/prisma/prismaGetBooks";
+import { PrismaDeleteBooks } from "@/infrastructure/database/prisma/prismaDeleteBooks";
 
 import { CreateBookController } from "../controllers/createBookController";
 import {
-  GetBooksByIdController,
   GetBooksController,
+  GetBooksByIdController,
 } from "../controllers/getBooksController";
+import {
+  DeleteBookController,
+  DeleteBooksByIdController,
+} from "../controllers/deleteBooksController";
 
+//Create
 const prismaCreateBooks = new PrismaCreateBooks();
 const createBookController = new CreateBookController(prismaCreateBooks);
 
+//Get
 const prismaGetBooks = new PrismaGetBooks();
 const getBooksController = new GetBooksController(prismaGetBooks);
 const getBooksByIdController = new GetBooksByIdController(prismaGetBooks);
 
-export { createBookController, getBooksController, getBooksByIdController };
+//Delete
+const prismaDeleteBooks = new PrismaDeleteBooks();
+const deleteBookController = new DeleteBookController(prismaDeleteBooks);
+const deleteBooksByIdController = new DeleteBooksByIdController(
+  prismaDeleteBooks,
+);
+
+export {
+  createBookController,
+  getBooksController,
+  getBooksByIdController,
+  deleteBookController,
+  deleteBooksByIdController,
+};
