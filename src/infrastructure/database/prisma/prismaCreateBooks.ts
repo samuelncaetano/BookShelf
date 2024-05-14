805333a42224a2d1772030923dcadca038b51690
samuelncaetano <samueln.caetano42@gmail.com>
2024-05-14 17:29:03 -0300

    feat(prisma): implement prisma to create many books

--- src/infrastructure/database/prisma/prismaCreateBooks.ts
+++ src/infrastructure/database/prisma/prismaCreateBooks.ts
@@ -10,7 +10,6 @@ const prisma = new PrismaClient();
 export class PrismaCreateBooks implements ICreateBooksRepository {
   async createBook(params: CreateBookParams): Promise<Book> {
     const status = params.status || "NOT_STARTED";
-
     const createdBook = await prisma.book.create({
       data: {
         title: params.title,
@@ -19,11 +18,36 @@ export class PrismaCreateBooks implements ICreateBooksRepository {
         status: status,
       },
     });
-
     if (!createdBook) {
       throw new Error("Book not created");
     }
-
     return createdBook;
+  }
+
+  async createManyBooks(params: CreateBookParams[]): Promise<Book[]> {
+    const booksToCreate = params.map((params) => {
+      const status = params.status || "NOT_STARTED";
+      return {
+        title: params.title,
+        author: params.author,
+        volume: params.volume,
+        status: status,
+      };
+    });
+
+    await prisma.book.createMany({
+      data: booksToCreate,
+    });
+
+    const createdBooks = await prisma.book.findMany({
+      where: {
+        OR: booksToCreate.map((book) => ({
+          title: book.title,
+          author: book.author,
+        })),
+      },
+    });
+
+    return createdBooks;
   }
 }
